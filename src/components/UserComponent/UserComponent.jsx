bd68c0235ff39795efc89cd3bf74c86ba40590a2
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-08-03 20:17:00 +0300

    lesson_1

--- src/components/UserComponent/UserComponent.jsx
+++ src/components/UserComponent/UserComponent.jsx
@@ -6,8 +6,8 @@ const UserComponent = (props) => {
             <h1>{name}</h1>
             <h1>{nickname}</h1>
             <h1>{email}</h1>
-            <h1>hello</h1>
-            <h2>lesson-1</h2>
+
+
         </div>
     )
 }
